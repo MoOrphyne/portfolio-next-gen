8f34b429621b3583b3b75b0efdbad471c625f219
delphine cretegny <delphine.cretegny@free.fr>
2019-09-19 00:42:37 +0200

    active effecy on navbar advanced

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,11 @@
 import "bootstrap";
+
+var header = document.getElementById("navbar-list");
+var btns = header.getElementsByClassName("nav-item");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace("active", "");
+  this.className += " active";
+  });
+}
